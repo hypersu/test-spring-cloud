c47ab2cec7b191fc0b76fafeed13fa94842d1a00
hypersu <623661487@qq.com>
2023-06-25 17:52:31 +0800

    测试loadBalancer组件

--- cloud-demo/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ cloud-demo/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -3,7 +3,10 @@ package com.example.orderservice.controller;
 import com.example.orderservice.pojo.Order;
 import com.example.orderservice.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
