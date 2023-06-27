e5db474531891f630376027b8d294a84e9cf5839
hypersu <623661487@qq.com>
2023-06-27 17:33:34 +0800

    测试nacos配置中心优先级、多环境共享

--- cloud-demo/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ cloud-demo/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,27 +1,36 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.config.PatternProperties;
 import com.example.userservice.pojo.User;
 import com.example.userservice.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/user")
-@RefreshScope
+//@RefreshScope
 public class UserController {
     @Autowired
     private IUserService userService;
-    @Value("${pattern.dateformat}")
-    private String dateformat;
+    //@Value("${pattern.dateformat}")
+    //private String dateformat;
+    @Autowired
+    private PatternProperties properties;
+
+    @GetMapping("prop")
+    public PatternProperties properties() {
+        return properties;
+    }
 
     @GetMapping("now")
-    public String now(){
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
     }
 
     @GetMapping("{id}")
