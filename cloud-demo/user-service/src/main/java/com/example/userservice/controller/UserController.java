3f29650e68f7df5f8cd0a342fd2c345a8f17e869
hypersu <623661487@qq.com>
2023-06-27 15:18:27 +0800

    测试nacos配置中心，配置热更新

--- cloud-demo/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ cloud-demo/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -3,13 +3,26 @@ package com.example.userservice.controller;
 import com.example.userservice.pojo.User;
 import com.example.userservice.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 public class UserController {
     @Autowired
     private IUserService userService;
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    }
 
     @GetMapping("{id}")
     public User getById(@PathVariable("id") String id) {
